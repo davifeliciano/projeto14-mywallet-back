dc735766227f54be4be09c8bef73c4c2890a0cde
Davi Feliciano <dfeliciano37@gmail.com>
2023-04-22 21:26:25 -0300

    Ajustes para deploy

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
 import express, { json } from "express";
 import cors from "cors";
 import router from "./routes/index.routes.js";
 
+dotenv.config();
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 app.use(cors());
 app.use(json());
 app.use(router);
