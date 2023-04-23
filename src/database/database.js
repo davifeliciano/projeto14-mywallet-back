3ae06ce22f13ef412228ae2bdeeff595396ec68d
Davi Feliciano <dfeliciano37@gmail.com>
2023-04-23 10:35:15 -0300

    Fix: using db name on env variable to allow usage of an atlas cluster

--- src/database/database.js
+++ src/database/database.js
@@ -4,8 +4,9 @@ import { MongoClient } from "mongodb";
 dotenv.config();
 
 const uri = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME;
 const client = new MongoClient(uri);
-const db = client.db();
+const db = client.db(dbName);
 
 try {
   db.command({ ping: 1 });
